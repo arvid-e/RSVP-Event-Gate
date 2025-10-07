2c0e9a3da1b2b81c7ef531fe6cf575e83b40a9de
arvid-e <arvid.edvinsson@gmail.com>
2025-10-07 14:46:32 +0900

    Add methods to event controller

---
+++ server/src/controllers/event.controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+
+export class EventController {
+  public createEvent = (req: Request, res: Response): void => {};
+
+  public getAllEvents = (req: Request, res: Response): void => {};
+
+  public getEventById = (req: Request, res: Response): void => {};
+
+  public updateEvent = (req: Request, res: Response): void => {};
+
+  public deleteEvent = (req: Request, res: Response): void => {};
+}
