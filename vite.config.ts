f83079607bacae697d68cf9e32de6c86eff8be87
arvid-e <arvid.edvinsson@gmail.com>
2025-10-07 14:07:42 +0900

    Setup React and Vite instead of just typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  test: {
-    globals: true,
-    coverage: {
-      provider: 'v8',
-      enabled: true,
-      reporter: ['text', 'html'],
-      include: ['src/**/*.ts'],
-    },
-  },
-});
+  plugins: [react()],
+})
