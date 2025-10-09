a2e6406977e7531210660ba8885e372af1045ee5
arvid-e <arvid.edvinsson@gmail.com>
2025-10-09 17:14:13 +0900

    Install and setup cors for frontend communication

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import router from './routes/router';
 
 export const app = express();
+
+const corsOptions = {
+    origin: 'http://localhost:5173', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
