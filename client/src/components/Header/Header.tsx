a332ca65287dd69e2e85f04e228350197e401ab7
arvid-e <arvid.edvinsson@gmail.com>
2025-10-09 17:26:23 +0900

    Remove react template code and start frontend setup

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+
+function Header() {
+    return(
+        <header>
+            <h1>RSVP Event Gate</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Events</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
