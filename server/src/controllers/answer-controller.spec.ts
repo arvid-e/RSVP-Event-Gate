c533eac954fb8772f46303a6000b677f619a8113
arvid-e <arvid.edvinsson@gmail.com>
2025-10-10 17:46:27 +0900

    Format code in answer controller test

--- server/src/controllers/answer-controller.spec.ts
+++ server/src/controllers/answer-controller.spec.ts
@@ -1,10 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
-import { beforeEach, describe, expect, it, Mocked, vi } from 'vitest';
-import {
-  AnswerData,
-  AnswerDataDocument,
-  AnswerServiceImp,
-} from '../types/answer-types';
+import { beforeEach, describe, Mocked, vi } from 'vitest';
+import { AnswerData } from '../types/answer-types';
+import { AnswerServiceImp } from '../services/answer-service';
 import { AnswerController } from './answer-controller';
 
 type MockAnswerServiceImp = Mocked<AnswerServiceImp>;
@@ -14,29 +11,36 @@ const mockAnswers: AnswerData[] = [
   { name: 'Tester2', email: 'tester2@test.com', date: '2020/02/02' },
 ];
 
-describe('AnswerServiceImp', () => {
-  let answerService: MockAnswerServiceImp;
-  let answerController: AnswerController;
+let answerService: MockAnswerServiceImp;
+let answerController: AnswerController;
 
-  beforeEach(() => {
-    vi.clearAllMocks();
+beforeEach(() => {
+  vi.clearAllMocks();
 
-    answerService = {
-      getAllAnswers: vi.fn(),
-      getAnswerById: vi.fn(),
-      createAnswer: vi.fn(),
-      deleteAnswer: vi.fn(),
-      updateAnswer: vi.fn(),
-    };
+  answerService = {
+    getAllAnswers: vi.fn(),
+    getAnswerById: vi.fn(),
+    createAnswer: vi.fn(),
+    deleteAnswer: vi.fn(),
+    updateAnswer: vi.fn(),
+  };
 
-    let mockReq: Partial<Request>;
-    let mockRes: Partial<Response>;
-    let mockNext: NextFunction;
+  let mockReq: Partial<Request>;
+  let mockRes: Partial<Response>;
+  let mockNext: NextFunction;
 
-    answerController = new AnswerController(answerService);
-  });
+  answerController = new AnswerController(answerService);
 
-  it('findAllAnswers() should return an array of answers', async () => {
+  mockNext = vi.fn();
+  mockRes = {
+    status: vi.fn(() => mockRes as Response),
+    json: vi.fn(),
+  };
 
-  });
+  // 4. Setup Mock Request Object (reset for each test)
+  mockReq = {};
+});
+
+describe('getAllAnswers()', async () => {
+
 });
