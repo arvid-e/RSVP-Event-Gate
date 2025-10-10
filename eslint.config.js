d5a8708f509868fb61356e6080915e683a8c17e9
arvid-e <arvid.edvinsson@gmail.com>
2025-10-10 12:51:01 +0900

    Fix lint config and make type guard for mongoose config error

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,52 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import reactRefreshPlugin from 'eslint-plugin-react-refresh';
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['**/dist/**', '**/build/**']),
+
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        jsxPragma: null,
+      },
     },
   },
-])
+
+  {
+    files: ['**/*.{jsx,tsx}'],
+
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+      'react-refresh': reactRefreshPlugin,
+    },
+
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off', 
+      'react-refresh/only-export-components': 'warn',
+    },
+  },
+]);
--- server/src/config/mongoose.ts
+++ server/src/config/mongoose.ts
@@ -1,21 +1,30 @@
 import mongoose from 'mongoose';
 
+function isErrorWithMessage(err: unknown): err is { message: string } {
+  return (
+    typeof err === 'object' &&
+    err !== null &&
+    'message' in err &&
+    typeof (err as { message: string }).message === 'string'
+  );
+}
+
 export const connectDB = async () => {
   try {
     const mongoURI = process.env.MONGO_URI;
 
     if (!mongoURI) {
-        throw Error('MONGO_URI not found!')
+      throw new Error('MONGO_URI environment variable not found!');
     }
-    console.log('Waiting for MongoDB...')
+    console.log('Waiting for MongoDB...');
+
     await mongoose.connect(mongoURI);
+
     console.log('MongoDB Connected.');
-
-
-  } catch (err: any) {
-    console.error('MongoDB connection error:', err.message);
-    process.exit(1);
+  } catch (err: unknown) {
+    if (isErrorWithMessage(err)) {
+      console.error('MongoDB connection error:', err.message);
+      process.exit(1);
+    }
   }
 };
-
-
